109fbf28af582752ef3712927abca3d0046ef6d9
Subir <subir.chowdhuri@gmail.com>
2022-05-27 04:04:48 +0530

    chore: path aliases

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,26 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@src": "./src",
+            "@atoms": "./src/components/atoms",
+            "@molecules": "./src/components/molecules",
+            "@organisms": "./src/components/organisms",
+            "@templates": "./src/components/templates",
+            "@constants": "./src/constants",
+            "@hooks": "./src/hooks",
+            "@pages": "./src/pages",
+            "@state": "./src/state",
+            "@types": "./src/types",
+            "@utils": "./src/utils",
+          },
+          extensions: [".ts", ".tsx"],
+        },
+      ],
+    ],
   };
 };
