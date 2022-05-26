282995399ccd9d9d59cd0ab4a952c328586170d6
Subir <subir.chowdhuri@gmail.com>
2022-05-27 04:05:27 +0530

    test: setup jest

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -7,7 +7,11 @@ interface MyButtonProps {
 
 export const MyButton = ({ onPress, text }: MyButtonProps) => {
   return (
-    <TouchableOpacity style={styles.container} onPress={onPress}>
+    <TouchableOpacity
+      accessibilityRole="button"
+      style={styles.container}
+      onPress={onPress}
+    >
       <Text style={styles.text}>{text} :)</Text>
     </TouchableOpacity>
   );
